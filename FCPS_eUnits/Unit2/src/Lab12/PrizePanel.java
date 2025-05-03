6f83706519ee4c6fb7cc5aef5fe4e5bab594141d
Tony940720 <tony940720@gmail.com>
2025-05-03 21:57:47 +0800

    202050503(week11)

--- FCPS_eUnits/Unit2/src/Lab12/PrizePanel.java
+++ FCPS_eUnits/Unit2/src/Lab12/PrizePanel.java
@@ -12,30 +12,58 @@ import java.awt.image.*;
    private Ball ball;
    private Polkadot pd;
    private Timer t; 
+   private int score;
 	//constructor   
     public PrizePanel()
    {
-      
-   }
-    public void paintComponent(Graphics g)
-   {
+      myImage =  new BufferedImage(FRAME, FRAME, BufferedImage.TYPE_INT_RGB);
+      myBuffer = myImage.getGraphics();
+      myBuffer.setColor(BACKGROUND);
+      myBuffer.fillRect(0, 0, FRAME,FRAME);
+      int xPos = (int)(Math.random()*(FRAME-100) + 50);
+      int yPos = (int)(Math.random()*(FRAME-100)+ 50);
+      ball = new Ball(xPos, yPos, 25, Color.RED);
+      pd = new Polkadot();
+      pd.setColor(Color.YELLOW);
+      t = new Timer(5, new Listener());
+      t.start();
    
    }
-    private class Listener implements ActionListener
+   public void paintComponent(Graphics g)
    {
-       public void actionPerformed(ActionEvent e)
+      g.drawImage(myImage, 0, 0, getWidth(), getHeight(), null);
+   }
+   private class Listener implements ActionListener
+   {
+      public void actionPerformed(ActionEvent e)
       {
-        
+         myBuffer.setColor(BACKGROUND);
+         myBuffer.fillRect(0, 0, FRAME, FRAME);
+         if(collide(ball, pd))
+         {
+            pd.jump(FRAME, FRAME);
+            score++;
+         }
+         pd.draw(myBuffer);
+         ball.move(FRAME,FRAME);
+         ball.draw(myBuffer);   
+         
+         myBuffer.setColor(Color.BLACK);
+         myBuffer.setFont(new Font("Monospaced", Font.BOLD, 24));
+         myBuffer.drawString("Count: " + score, FRAME - 150, 25);  
+         repaint();   
       }
    }   
-    private void collide(Ball b, Polkadot pd)
+    private boolean collide(Ball b, Polkadot pd)
    {
-     double d = distance(  /* 4 arguments */  );  
-	
-	  
+      double d = distance(b.getX(), b.getY(), pd.getX(), pd.getY());  
+      if(d <= b.getRadius() + pd.getRadius())
+         return true;
+      return false;     
    }
     private double distance(double x1, double y1, double x2, double y2)
    {
-      return  	 // enter the calculation here.
+      double x = Math.sqrt(Math.pow((x1 - x2), 2.0) + Math.pow((y1 - y2), 2.0));
+      return   x; 
    }
 }
